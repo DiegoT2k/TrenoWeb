5125a096bb1f06c44bc8b99f7cdd41003e7a7d7d
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-07-31 16:13:50 +0200

    Add Controller and Servlet

--- TrenoWeb/src/main/java/com/corso/controller/TrenoController.java
+++ TrenoWeb/src/main/java/com/corso/controller/TrenoController.java
@@ -3,8 +3,17 @@ package com.corso.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TrenoController {
-
+	
+	@GetMapping("/index")
+	 public String index(){   
+	   
+		System.out.println("sono passato dal metodo controller index()!"); 	
+		
+      return "index";  
+  }  	
+	
 }
