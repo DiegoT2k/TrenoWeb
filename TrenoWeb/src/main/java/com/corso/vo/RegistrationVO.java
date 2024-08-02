fde54cbd3e2b3946e09aa24d18eb629365879409
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-08-02 10:16:20 +0200

    Fix Registration

---
+++ TrenoWeb/src/main/java/com/corso/vo/RegistrationVO.java
@@ -0,0 +1,45 @@
+package com.corso.vo;
+
+public class RegistrationVO {
+	
+	private String nome;
+	private String cognome;
+	private String username;
+	private String email;
+	private String password;
+	
+	public RegistrationVO() {
+			
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
