dcce153c5b4b8908efa0a337e4d733f1c8ab207c
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-08-06 09:10:00 +0200

    Fix filtri

--- TrenoWeb/src/main/java/com/corso/service/impl/ValutazioneServiceImpl.java
+++ TrenoWeb/src/main/java/com/corso/service/impl/ValutazioneServiceImpl.java
@@ -1,7 +1,6 @@
 package com.corso.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.corso.dao.ValutazioneDao;
 import com.corso.service.ValutazioneService;
 
@@ -12,7 +11,11 @@ public class ValutazioneServiceImpl implements ValutazioneService{
 	
 	@Override
 	public void addVoto(int rating, int id_treno, int id_utente) {
+		
+		valutazioneDao.findVoto(id_treno, id_utente);
+		
 		valutazioneDao.addVoto(rating, id_treno, id_utente);
+		
 	}
 	
 }
